d2324a20f2d411ef00cfad4249f787fdf31a0d36
ernaratna <75685305+ernaratna@users.noreply.github.com>
2020-12-08 23:08:45 +0700

    Add files via upload

---
+++ data/titik_acuan_1.js
@@ -0,0 +1,8 @@
+var json_titik_acuan_1 = {
+"type": "FeatureCollection",
+"name": "titik_acuan_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "Point", "coordinates": [ 110.667330865575835, -6.590619617388108 ] } }
+]
+}
